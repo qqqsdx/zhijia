393ac517ef61131e100ab3eb39c5737eab8679bb
zzz-zzz9 <1104654719@qq.com>
2020-09-01 22:28:06 +0800

    wode-901

--- src/router/index.js
+++ src/router/index.js
@@ -3,33 +3,39 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
-    {
-      path: '/',
-      redirect: '/index'
+const routes = [{
+        path: '/',
+        redirect: '/index'
     },
     {
-      path: '/home',
-      component: () => import('../views/Home.vue'),
-      children: [
-        {
-          path: '/index',
-          component: () => import('../views/index.vue'),
-        },
-      ]
+        path: '/home',
+        component: () =>
+            import ('../views/Home.vue'),
+        children: [{
+                path: '/index',
+                component: () =>
+                    import ('../views/index.vue'),
+            },
+            {
+                path: '/my',
+                component: () =>
+                    import ('../views/My.vue'),
+            },
+        ]
     },
     // {
     //   path: '/detail/:id',
     //   component: () => import('../views/Detail.vue')
     // },
     {
-      path: '*',
-      component: () => import('../views/Nofound404.vue'),
+        path: '*',
+        component: () =>
+            import ('../views/Nofound404.vue'),
     }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
