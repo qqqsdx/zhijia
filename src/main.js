ab9ba9344bbc6428954008d32fa73acadd149d33
likx1998 <18868537180@163.com>
2020-09-02 08:49:57 +0800

    做了体验模块

--- src/main.js
+++ src/main.js
@@ -2,15 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import "amfe-flexible"
 // 全局引入Vant Ui
 import Vant from "vant";
+import "./assets/style/reset.css";
 import "vant/lib/index.css";
 Vue.use(Vant);
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
