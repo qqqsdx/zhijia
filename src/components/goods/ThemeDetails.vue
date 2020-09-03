6d8ca526a644279028a039121beac1e75637afde
Xt-wml <3526844055@qq.com>
2020-09-03 21:14:28 +0800

    123

---
+++ src/components/goods/ThemeDetails.vue
@@ -0,0 +1,86 @@
+<!--主题推荐-->
+
+<template>
+    <div class="themedetails-container">
+        <div class="header">
+            <van-nav-bar
+                title="主题详情"
+                left-arrow
+                @click-left="onClickLeft"
+            />
+        </div>
+
+
+        <div class="content">
+            <div class="img1">
+                <img src="../../assets/goods/themedetails/header.png" alt="">
+            </div>
+            <p class="title">适合小户型的8个家居单品</p>
+            <div class="img2">
+                <!-- <img src="../../assets/goods/themedetails/矩形 1.png" alt=""> -->
+                1111
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        onClickLeft() {
+            this.$router.go(-1)
+        }
+    },
+}
+</script>
+
+
+<style lang="scss">
+    .themedetails-container{
+        .header{
+            // width: 100%;
+            // height: 50px;
+            // background: pink;
+            .van-icon{
+                color: #000;
+            }
+            .van-ellipsis{
+                color: #333333;
+                font-size: 16px;
+            }
+        }
+        .content{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            .img1{
+                width: 100%;
+                height: 185px;
+                
+                img{
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+            .title{
+                margin-top: 30px;
+                width: 219px;
+                height: 18px;
+                font-size: 18px;
+                color: #333;
+            }
+            .img2{
+                margin-top: 18px;
+                width: 343px;
+                height: 46px;
+                background: url('../../assets/goods/themedetails/矩形 1.png');
+            }
+        }
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,134 +4,186 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [{
-  path: '/',
-  redirect: '/index'
-},
-{
-  path: '/home',
-  component: () => import('../views/Home.vue'),
-  children: [
-    {
-      //首页
-      path: '/index',
-      component: () => import('../views/index.vue'),
-    },
-    //--------------------------index服务页面子页面--------------------------------
-    {
-      path:'/index/serve/:id',
-      component: () => import('../components/index/serve/serves.vue'),
-    },
-    {
-      path: '/home',
-      component: () => import('../views/Home.vue'),
-      children: [
-        {
-        // ------------------------------首页--------------------------------------
-          path: '/index',
-          component: () => import('../views/index.vue'),
+            path: '/',
+            redirect: '/index'
         },
-        // ------------------------------show页面----------------------------------
+        // 主题页面的商品列表--------------------------------------------------------------------------
         {
-        //
-          path: '/show',
-          component: () => import('../views/show.vue'),
-        },
-        //--------------------------------体验------------------------------------
-        {
-            path: '/experience',
+
+            path: '/theme/themegoods',
             component: () =>
-                import ('../views/Experience.vue')
-        },
-        //------------------------------商品路由----------------------------------------
-        {
-            path: '/commodity',
-            redirect: '/commodity/content'
+                import ('../components/goods/ThemeGoods.vue')
+
         },
         {
-            path: '/commodity',
+
+            path: '/theme/themedetails',
             component: () =>
-                import ('../views/Commodity.vue'),
+                import ('../components/goods/ThemeDetails.vue')
+
+        },
+
+        // --------------------------------------------------------------------------------------------
+        {
+            path: '/home',
+            component: () =>
+                import ('../views/Home.vue'),
             children: [{
-                    path: 'content',
+                    //首页
+                    path: '/index',
                     component: () =>
-                        import ('../components/goods/Content.vue')
+                        import ('../views/index.vue'),
+                },
+                //--------------------------index服务页面子页面--------------------------------
+                {
+                    path: '/index/serve/:id',
+                    component: () =>
+                        import ('../components/index/serve/serves.vue'),
                 },
                 {
-                    path: 'theme',
+                    path: '/home',
                     component: () =>
-                        import ('../components/goods/Theme.vue')
+                        import ('../views/Home.vue'),
+                    children: [{
+                            // ------------------------------首页--------------------------------------
+                            path: '/index',
+                            component: () =>
+                                import ('../views/index.vue'),
+                        },
+                        // ------------------------------show页面----------------------------------
+                        {
+                            //
+                            path: '/show',
+                            component: () =>
+                                import ('../views/show.vue'),
+                        },
+                        //--------------------------------体验------------------------------------
+                        {
+                            path: '/experience',
+                            component: () =>
+                                import ('../views/Experience.vue')
+                        },
+                        //------------------------------商品路由----------------------------------------
+                        {
+                            path: '/commodity',
+                            redirect: '/commodity/content'
+                        },
+                        {
+                            path: '/commodity',
+                            component: () =>
+                                import ('../views/Commodity.vue'),
+                            children: [{
+                                    path: 'content',
+                                    component: () =>
+                                        import ('../components/goods/Content.vue')
+                                },
+                                {
+                                    path: 'theme',
+                                    component: () =>
+                                        import ('../components/goods/Theme.vue'),
+                                    // children: [{
+                                    //     path: '/themegoods',
+                                    //     component: () =>
+                                    //         import ('../components/goods/ThemeGoods.vue')
+                                    // }]
+                                }
+                            ]
+                        },
+
+                        //商品列表
+                        {
+                            path: '/goodslist',
+                            component: () =>
+                                import ('../components/goods/GoodsList.vue'),
+                            children: [{
+                                    path: '/goodslist',
+                                    redirect: '/goodslist/goods1'
+                                },
+                                {
+                                    path: '/goodslist/goods0',
+                                    component: () =>
+                                        import ('../components/goods/Goods0.vue')
+                                },
+                                {
+                                    path: '/goodslist/goods1',
+                                    component: () =>
+                                        import ('../components/goods/Goods1.vue')
+                                },
+                                {
+                                    path: '/goodslist/goods2',
+                                    component: () =>
+                                        import ('../components/goods/Goods2.vue')
+                                }, {
+                                    path: '/goodslist/goods3',
+                                    component: () =>
+                                        import ('../components/goods/Goods3.vue')
+                                }
+                            ]
+                        },
+                        {
+                            // ---------------------------------我的页面--------------------------------
+                            //
+                            path: '/my',
+                            component: () =>
+                                import ('../views/my.vue'),
+                        },
+                    ]
+                },
+                //---------------------------------下面是登录页的东西-----------------------
+                {
+                    path: '/logandreg',
+                    component: () =>
+                        import ('../views/LogAndReg/default.vue'),
+                },
+                {
+                    name: 'agreement',
+                    path: '/logandreg/agreement',
+                    component: () =>
+                        import ('../views/LogAndReg/agreement.vue'),
+                },
+                {
+                    name: 'privacy',
+                    path: '/logandreg/privacy',
+                    component: () =>
+                        import ('../views/LogAndReg/privacy.vue'),
+                },
+                {
+                    name: 'log',
+                    path: '/logandreg/log',
+                    component: () =>
+                        import ('../views/LogAndReg/log.vue'),
+                },
+                // {
+                //   path: '/detail/:id',
+                //   component: () => import('../views/Detail.vue')
+                // },
+                {
+                    path: '/more',
+                    component: () =>
+                        import ('../views/More.vue')
+                },
+                {
+                    path: '*',
+                    component: () =>
+                        import ('../views/Nofound404.vue'),
                 }
             ]
-        },
-        //商品列表
-        {
-            path: '/goodslist',
-            component: () =>
-                import ('../components/goods/GoodsList.vue')
-        },
-        {
-        // ---------------------------------我的页面--------------------------------
-        //
-          path: '/my',
-          component: () => import('../views/my.vue'),
-        },
-        ]},
-        //---------------------------------下面是登录页的东西-----------------------
-        {
-            path: '/logandreg',
-            component: () => import('../views/LogAndReg/default.vue'),
-          },
-          {
-            name: 'agreement',
-            path: '/logandreg/agreement',
-            component: () => import('../views/LogAndReg/agreement.vue'),
-          },
-          {
-            name: 'privacy',
-            path: '/logandreg/privacy',
-            component: () => import('../views/LogAndReg/privacy.vue'),
-          },
-          {
-            name: 'log',
-            path: '/logandreg/log',
-            component: () => import('../views/LogAndReg/log.vue'),
-          },
-    // {
-    //   path: '/detail/:id',
-    //   component: () => import('../views/Detail.vue')
-    // },
-    {
-        path: '/more',
-        component: () =>
-            import ('../views/More.vue')
-    },
-    {
-        path: '*',
-        component: () =>
-            import ('../views/Nofound404.vue'),
-    }
-]
-}]//最大的一层
+        }
+    ] //最大的一层
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 //全局守卫（暂时不用），判断进入此页面是否需要登录状态。如果需要登录，请在对应的路由中写上meta:{requireAuth:true},表示进入这个路由需要验证
 router.beforeEach((to, from, next) => {
-  const token = localStorage.getItem("token")
-  if (to.matched.some(record => record.meta.requireAuth)) {
-    if (!token) {
-      next({
-        path: '/logandreg', query: { redirect: to.fullPath }
-      })
-    } else {
-      if (Object.keys(from.query).length === 0) {//判断路由来源是否有query，处理不是目的跳转的情况
-        next()
-      } else {
-        let redirect = from.query.redirect//如果来源路由有query
-        if (to.path === redirect) {//这行是解决next无限循环的问题
-          next()
+    const token = localStorage.getItem("token")
+    if (to.matched.some(record => record.meta.requireAuth)) {
+        if (!token) {
+            next({
+                path: '/logandreg',
+                query: { redirect: to.fullPath }
+            })
         } else {
             if (Object.keys(from.query).length === 0) { //判断路由来源是否有query，处理不是目的跳转的情况
                 next()
@@ -140,14 +192,21 @@ router.beforeEach((to, from, next) => {
                 if (to.path === redirect) { //这行是解决next无限循环的问题
                     next()
                 } else {
-                    next({ path: redirect }) //跳转到目的路由
+                    if (Object.keys(from.query).length === 0) { //判断路由来源是否有query，处理不是目的跳转的情况
+                        next()
+                    } else {
+                        let redirect = from.query.redirect //如果来源路由有query
+                        if (to.path === redirect) { //这行是解决next无限循环的问题
+                            next()
+                        } else {
+                            next({ path: redirect }) //跳转到目的路由
+                        }
+                    }
                 }
             }
         }
     } else {
         next()
     }
-  }
-}
 })
 export default router
